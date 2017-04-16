0e54b494bf276a73550d27419691f651ecb15c48
Jesus Hilario <jesushilariohernandez@gmail.com>
2017-04-16 00:28:02 -0500

    modified index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
     var mySwiper = new Swiper ('.swiper-container', {
       // Optional parameters
       pagination: '.swiper-pagination',
-      paginationClickable: true
+      paginationClickable: true,
+      spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 3000,
+        autoplayDisableOnInteraction: false,
+        effect: 'fade'
     })
   });
